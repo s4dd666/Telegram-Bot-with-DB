839942040d1d8342a9eaf7b45f2407c61a7b4fe2
Viktor Robulets <116356981+s4dd666@users.noreply.github.com>
2022-11-23 01:14:30 +0200

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -8,28 +8,27 @@ from config import db
 
 
 class BaseModel(Model):
-	user_id = PrimaryKeyField(column_name='ID', unique=True)
-	telegram_id = CharField(column_name='Telegram ID', unique=True)
+	telegram_id = PrimaryKeyField(column_name='Telegram ID', unique=True)
 	user_name = CharField(column_name='Nickname', null=True)
 	first_name = CharField(column_name='First Name', null=True)
 
-
 	class Meta:
 		database = db
-		order_by = 'user_id'
 
 class User(BaseModel):
-	last_name = CharField(column_name='Last name', null=True)
-	date_time_join = DateTimeField(column_name='Date time join',
-	                               default=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+	user_id = PrimaryKeyField(column_name='ID', unique=True)
+	telegram_id = ForeignKeyField(BaseModel, column_name='Telegram ID', unique=True)
+	last_name = CharField(column_name='Last Name', null=True)
+	date_time_join = DateTimeField(column_name='Date time join', default=now)
 
 	class Meta:
 		db_table = 'users'
 
 class Message(BaseModel):
+	message_id = PrimaryKeyField(column_name='Message ID', unique=True)
+	telegram_id = ForeignKeyField(BaseModel, column_name='Telegram ID', unique=False)
 	message_text = CharField(column_name='Text', null=False)
-	date_time_add = DateTimeField(column_name='Date time add',
-	                              default=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+	date_time_add = DateTimeField(column_name='Date time add', default=now)
 
 	class Meta:
 		db_table = 'messages'
